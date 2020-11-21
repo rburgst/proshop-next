364b61ca983818b5b1bd849400b47e10f698fa8a
Rainer Burgstaller <rburgst@gmail.com>
2020-11-21 10:01:07 +0100

    add cart page functionality
    - change qty, etc.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import {AppProps} from "next/app";
+import { AppProps } from "next/app";
 import "../styles/globals.css";
 import "../styles/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/js/fontawesome";
@@ -7,29 +7,31 @@ import "@fortawesome/fontawesome-free/js/regular";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import React from "react";
-import {Provider} from "react-redux";
-import {useStore} from "../frontend/store";
+import { Provider } from "react-redux";
+import { useStore } from "../frontend/store";
 
-import {persistStore} from 'redux-persist'
-import {PersistGate} from 'redux-persist/integration/react'
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import { Container } from "react-bootstrap";
 
-function MyApp({Component, pageProps}: AppProps) {
-    const store = useStore(pageProps.initialReduxState)
-    const persistor = persistStore(store, {}, function () {
-        persistor.persist()
-    })
-    return (
-        <Provider store={store}>
-            <PersistGate loading={<div>loading</div>} persistor={persistor}>
-
-                <Header/>
-                <main className="py-3">
-                    <Component {...pageProps} />
-                </main>
-                <Footer/>
-            </PersistGate>
-        </Provider>
-    );
+function MyApp({ Component, pageProps }: AppProps) {
+  const store = useStore(pageProps.initialReduxState);
+  const persistor = persistStore(store, {}, function () {
+    persistor.persist();
+  });
+  return (
+    <Provider store={store}>
+      <PersistGate loading={<div>loading</div>} persistor={persistor}>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Component {...pageProps} />
+          </Container>
+        </main>
+        <Footer />
+      </PersistGate>
+    </Provider>
+  );
 }
 
 export default MyApp;
--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,7 +1,20 @@
 import Link from "next/link";
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useCallback } from "react";
+import {
+  Button,
+  Card,
+  Col,
+  Form,
+  Image,
+  ListGroup,
+  Row,
+} from "react-bootstrap";
 import { useSelector } from "react-redux";
-import { RootState } from "../../frontend/store";
+import Message from "../../components/Message";
+import { RootState, useAppDispatch } from "../../frontend/store";
+import { addToCart } from "../../frontend/reducers/cartReducers";
+import { Variant } from "react-bootstrap/esm/types";
+import { useRouter } from "next/router";
 
 interface CartScreenRouteParams {
   id: string;
@@ -10,19 +23,106 @@ interface CartScreenProps {}
 
 const CartScreen: FunctionComponent<CartScreenProps> = () => {
   const cartItems = useSelector((state: RootState) => state.cart.cartItems);
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+
+  const onRemoveFromCartHandler = useCallback(
+    (id) => {
+      console.log("removing", id);
+    },
+    [dispatch]
+  );
+  const checkoutHandler = useCallback(() => {
+    console.log("checkout");
+    router.push("/login?redirect=shipping");
+  }, [router]);
+
   return (
     <>
       <Link href="/">
         <a className="btn btn-light my-3">Go Back</a>
       </Link>
-      <h3>Cart Overview {cartItems.length} items</h3>
-      <ul>
-        {cartItems.map((item) => (
-          <li>
-            {item.name}: {item.qty}
-          </li>
-        ))}
-      </ul>
+      <Row>
+        <Col md={8}>
+          <h1>Cart Overview {cartItems.length} items</h1>
+          {cartItems.length === 0 ? (
+            <Message>
+              Your cart is empty. <Link href="/">Go Back</Link>
+            </Message>
+          ) : (
+            <ListGroup variant="flush">
+              {cartItems.map((item) => (
+                <ListGroup.Item key={item.product}>
+                  <Row>
+                    <Col md={2}>
+                      <Image src={item.image} alt={item.name} fluid rounded />
+                    </Col>
+                    <Col md={3}>
+                      <Link href={`products/${item.product}`}>{item.name}</Link>
+                    </Col>
+                    <Col md={2}>${item.price}</Col>
+                    <Col md={2}>
+                      <Form.Control
+                        as="select"
+                        value={item.qty}
+                        onChange={(e) =>
+                          dispatch(
+                            addToCart({
+                              productId: item.product,
+                              qty: Number(e.target.value),
+                            })
+                          )
+                        }
+                      >
+                        {[...Array(item.countInStock).keys()].map((x) => (
+                          <option key={x + 1} value={x + 1}>
+                            {x + 1}
+                          </option>
+                        ))}
+                      </Form.Control>
+                    </Col>
+                    <Col md={2}>
+                      <Button
+                        variant="light"
+                        onClick={() => onRemoveFromCartHandler(item.product)}
+                      >
+                        <i className="fas fa-trash"></i>
+                      </Button>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          )}
+        </Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h2>
+                  Subtotal ({cartItems.reduce((acc, cur) => acc + cur.qty, 0)})
+                  items
+                </h2>
+                $
+                {cartItems
+                  .reduce((acc, cur) => acc + cur.price * cur.qty, 0)
+                  .toFixed(2)}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  type="button"
+                  className="btn-block"
+                  disabled={cartItems.length === 0}
+                  onClick={checkoutHandler}
+                >
+                  Proceed to Checkout
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+      <ul></ul>
     </>
   );
 };
