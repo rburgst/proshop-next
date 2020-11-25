9137b796e63b951c76d8b0d6890ade1fc89af95c
Rainer Burgstaller <rburgst@gmail.com>
2020-11-25 06:25:31 +0100

    add backend update user profile

--- pages/api/users/profile.ts
+++ pages/api/users/profile.ts
@@ -1,15 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiResponse } from "next";
 import connectDB from "../../../server/config/db";
-import User from "../../../server/models/userModel";
-import withMiddleware from "../../../server/middlewares/index";
-import { IUserDoc } from "../../../server/models/userModel";
-import { generateToken } from "../../../server/utils/generateToken";
 import {
-  protect,
   NextApiRequestWithUser,
+  protect,
 } from "../../../server/middlewares/authMiddleware";
+import withMiddleware from "../../../server/middlewares/index";
+import User, { IUserDoc } from "../../../server/models/userModel";
 
 const handler = async (req: NextApiRequestWithUser, res: NextApiResponse) => {
   console.info("get user profile");
@@ -26,9 +24,52 @@ const handler = async (req: NextApiRequestWithUser, res: NextApiResponse) => {
         isAdmin: user.isAdmin,
       });
     } else {
-      // Handle any other HTTP method
+      // user not found
       res.statusCode = 404;
       throw new Error("user not found");
+    }
+  } else if (req.method === "PUT") {
+    const { name, email, password } = req.body;
+    const mongoose = await connectDB();
+
+    const user: IUserDoc = await User.findById(req.user!._id);
+
+    if (!user) {
+      res.statusCode = 404;
+      throw new Error("user not found");
+    }
+
+    if (email && user.email !== email) {
+      const userExists = await User.findOne({ email });
+
+      if (userExists) {
+        res.statusCode = 400;
+        throw new Error("this email already exists");
+      }
+    }
+
+    if (email) {
+      user.email = email;
+    }
+    if (name) {
+      user.name = name;
+    }
+    if (password) {
+      user.password = password;
+    }
+
+    const updatedUser = await user.save();
+    if (updatedUser) {
+      res.statusCode = 200;
+      res.json({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        isAdmin: updatedUser.isAdmin,
+      });
+    } else {
+      res.statusCode = 400;
+      throw new Error("invalid user data");
     }
   } else {
     // Handle any other HTTP method
