786b6f34ba4df007aaf1df71df6c22558f195159
Rainer Burgstaller <rburgst@gmail.com>
2020-11-26 08:33:18 +0100

    use next-redux-wrapper for store

--- frontend/store.ts
+++ frontend/store.ts
@@ -3,35 +3,40 @@ import {
   AnyAction,
   combineReducers,
   configureStore,
+  createStore,
   DeepPartial,
   EnhancedStore,
   getDefaultMiddleware,
+  Reducer,
   ThunkAction,
 } from "@reduxjs/toolkit";
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import { cartSlice } from "./reducers/cartReducers";
-import {
-  productListSlice,
-  productDetailsSlice,
-} from "./reducers/productReducers";
-import { persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import { useMemo } from "react";
 import { ThunkMiddlewareFor } from "@reduxjs/toolkit/src/getDefaultMiddleware";
-import { userUpdateProfileSlice } from "./reducers/userReducers";
-import {
-  userLoginSlice,
-  userRegisterSlice,
-  userDetailsSlice,
-} from "./reducers/userReducers";
+import { useMemo } from "react";
+import { useDispatch } from "react-redux";
 import {
   FLUSH,
   PAUSE,
   PERSIST,
+  PersistConfig,
+  persistReducer,
+  persistStore,
   PURGE,
   REGISTER,
   REHYDRATE,
 } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { cartSlice } from "./reducers/cartReducers";
+import {
+  productDetailsSlice,
+  productListSlice,
+} from "./reducers/productReducers";
+import {
+  userDetailsSlice,
+  userLoginSlice,
+  userRegisterSlice,
+  userUpdateProfileSlice,
+} from "./reducers/userReducers";
+import { MakeStore, createWrapper, Context, HYDRATE } from "next-redux-wrapper";
 
 export type StoreType = EnhancedStore<
   RootState,
@@ -41,39 +46,53 @@ export type StoreType = EnhancedStore<
 
 let store: StoreType;
 
-const cartPersistConfig = {
-  key: "cart",
-  storage,
-  whitelist: ["cartItems", "shippingAddress"], // place to select which state you want to persist
-};
-const userLoginPersistConfig = {
-  key: "userLogin",
-  storage,
-  whitelist: ["userInfo"], // place to select which state you want to persist
-};
-const rootReducer = combineReducers({
-  productList: productListSlice.reducer,
-  productDetails: productDetailsSlice.reducer,
-  cart: persistReducer(cartPersistConfig, cartSlice.reducer),
-  userLogin: persistReducer(userLoginPersistConfig, userLoginSlice.reducer),
-  userRegister: userRegisterSlice.reducer,
-  userDetails: userDetailsSlice.reducer,
-  userUpdateProfile: userUpdateProfileSlice.reducer,
-});
+function optionalPersistReducer<S, A extends Action = AnyAction>(
+  isServer: boolean,
+  reducer: Reducer<S, A>,
+  persistConfig: PersistConfig<S>
+): Reducer<S, A> {
+  return isServer ? reducer : persistReducer(persistConfig, reducer);
+}
 
-export type RootState = ReturnType<typeof rootReducer>;
+function createReducer(isServer: boolean): Reducer {
+  const cartPersistConfig = {
+    key: "cart",
+    storage,
+    whitelist: ["cartItems", "shippingAddress"], // place to select which state you want to persist
+  };
+  const userLoginPersistConfig = {
+    key: "userLogin",
+    storage,
+    whitelist: ["userInfo"], // place to select which state you want to persist
+  };
+  const rootReducer = combineReducers({
+    productList: productListSlice.reducer,
+    productDetails: productDetailsSlice.reducer,
+    cart: optionalPersistReducer(
+      isServer,
+      cartSlice.reducer,
+      cartPersistConfig
+    ),
+    userLogin: optionalPersistReducer(
+      isServer,
+      userLoginSlice.reducer,
+      userLoginPersistConfig
+    ),
+    userRegister: userRegisterSlice.reducer,
+    userDetails: userDetailsSlice.reducer,
+    userUpdateProfile: userUpdateProfileSlice.reducer,
+  });
+  return rootReducer;
+}
 
-const rootPersistConfig = {
-  key: "root",
-  storage,
-  blacklist: ["cart"], // place to select which state you want to persist
-};
+const dummyServerReducer = createReducer(true);
 
-// const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
+export type RootState = ReturnType<typeof dummyServerReducer>;
 
-function makeStore(initialState: DeepPartial<RootState>): StoreType {
-  const enhancedStore = (configureStore<RootState>({
-    reducer: rootReducer,
+const makeStore = () => {
+  const isServer = typeof window === "undefined";
+  const store = (configureStore<RootState>({
+    reducer: createReducer(isServer),
     // @ts-ignore: cant be bothered right now
     middleware: getDefaultMiddleware<RootState>({
       thunk: true,
@@ -84,40 +103,65 @@ function makeStore(initialState: DeepPartial<RootState>): StoreType {
       },
     }),
     devTools: true,
-    preloadedState: initialState,
+    //preloadedState: initialState,
   }) as unknown) as StoreType;
-  return enhancedStore;
-}
 
-export const initializeStore = (
-  preloadedState: DeepPartial<RootState>
-): StoreType => {
-  let _store: StoreType = store ?? makeStore(preloadedState);
-
-  // After navigating to a page with an initial Redux state, merge that state
-  // with the current state in the store, and create a new store
-  if (preloadedState && store) {
-    let combinedInitialState = {
-      ...store.getState(),
-      ...preloadedState,
-    } as DeepPartial<RootState>;
-    _store = makeStore(combinedInitialState);
-    // Reset the current store
-    store = undefined;
+  if (!isServer) {
+    // @ts-ignore: this will be ok
+    store.__persistor = persistStore(store); // This creates a persistor object & push that persisted object to .__persistor, so that we can avail the persistability feature
   }
-
-  // For SSG and SSR always create a new store
-  if (typeof window === "undefined") return _store;
-  // Create the store once in the client
-  if (!store) store = _store;
-
-  return _store;
+  return store;
 };
 
-export function useStore(initialState: DeepPartial<RootState>) {
-  const store = useMemo(() => initializeStore(initialState), [initialState]);
-  return store;
-}
+// function makeStore(initialState: DeepPartial<RootState>): StoreType {
+//   const enhancedStore = (configureStore<RootState>({
+//     reducer: rootReducer,
+//     // @ts-ignore: cant be bothered right now
+//     middleware: getDefaultMiddleware<RootState>({
+//       thunk: true,
+//       immutableCheck: true,
+//       // @ts-ignore: wrong typescript for this
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }),
+//     devTools: true,
+//     preloadedState: initialState,
+//   }) as unknown) as StoreType;
+//   return enhancedStore;
+// }
+
+// export const initializeStore = (
+//   preloadedState: DeepPartial<RootState>
+// ): StoreType => {
+//   let _store: StoreType = store ?? makeStore(preloadedState);
+
+//   // After navigating to a page with an initial Redux state, merge that state
+//   // with the current state in the store, and create a new store
+//   if (preloadedState && store) {
+//     let combinedInitialState = {
+//       ...store.getState(),
+//       ...preloadedState,
+//     } as DeepPartial<RootState>;
+//     _store = makeStore(combinedInitialState);
+//     // Reset the current store
+//     store = undefined;
+//   }
+
+//   // For SSG and SSR always create a new store
+//   if (typeof window === "undefined") return _store;
+//   // Create the store once in the client
+//   if (!store) store = _store;
+
+//   return _store;
+// };
+
+// export function useStore(initialState: DeepPartial<RootState>) {
+//   const store = useMemo(() => initializeStore(initialState), [initialState]);
+//   return store;
+// }
+
+export const wrapper = createWrapper<RootState>(makeStore, { debug: true });
 
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,37 +1,36 @@
-import { AppProps } from "next/app";
 import "../styles/globals.css";
 import "../styles/bootstrap.min.css";
+
 import "@fortawesome/fontawesome-free/js/fontawesome";
-import "@fortawesome/fontawesome-free/js/solid";
 import "@fortawesome/fontawesome-free/js/regular";
+import "@fortawesome/fontawesome-free/js/solid";
+import { AppProps } from "next/app";
+import React from "react";
+import { useStore } from "react-redux";
+import { Persistor, persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import React from "react";
-import { Provider } from "react-redux";
-import { useStore } from "../frontend/store";
-
-import { persistStore } from "redux-persist";
-import { PersistGate } from "redux-persist/integration/react";
+import { wrapper } from "../frontend/store";
+import "../styles/bootstrap.min.css";
+import "../styles/globals.css";
 import { Container } from "react-bootstrap";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const store = useStore(pageProps.initialReduxState);
-  const persistor = persistStore(store, {}, function () {
-    persistor.persist();
-  });
+  const store = useStore();
+  // @ts-ignore we put it in the config before
+  const persistor: Persistor = store.__persistor;
   return (
-    <Provider store={store}>
-      <PersistGate loading={<div>loading</div>} persistor={persistor}>
-        <Header />
-        <main className="py-3">
-          <Container>
-            <Component {...pageProps} />
-          </Container>
-        </main>
-        <Footer />
-      </PersistGate>
-    </Provider>
+    <PersistGate persistor={persistor} loading={<div>Loading</div>}>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      </main>
+      <Footer />
+    </PersistGate>
   );
 }
 
-export default MyApp;
+export default wrapper.withRedux(MyApp);
