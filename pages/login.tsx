78afb6e400746eaa82cf82819ede8eee17e9bde1
Rainer Burgstaller <rburgst@gmail.com>
2020-11-30 08:14:57 +0100

    fix bug where the login screen was shown while logging in

--- pages/login.tsx
+++ pages/login.tsx
@@ -41,40 +41,43 @@ const LoginScreen: FunctionComponent<LoginScreenProps> = () => {
     <FormContainer>
       <h1>Sign in</h1>
       {error && <Message variant="danger">{error}</Message>}
-      {loading && <Loader />}
-      <Form onSubmit={submitHandler}>
-        <Form.Group controlId="email">
-          <Form.Label>Email Address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-        <Form.Group controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-        <Button type="submit" variant="primary">
-          Sign in
-        </Button>
-        <Row className="py-3">
-          <Col>
-            New Customer?{" "}
-            <Link
-              href={redirect ? `/register?redirect=${redirect}` : `/register`}
-            >
-              Register
-            </Link>
-          </Col>
-        </Row>
-      </Form>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="email">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Button type="submit" variant="primary">
+            Sign in
+          </Button>
+          <Row className="py-3">
+            <Col>
+              New Customer?{" "}
+              <Link
+                href={redirect ? `/register?redirect=${redirect}` : `/register`}
+              >
+                Register
+              </Link>
+            </Col>
+          </Row>
+        </Form>
+      )}
     </FormContainer>
   );
 };
