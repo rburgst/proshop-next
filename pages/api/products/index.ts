b373cbebf8b08c4d9d87b7e15b02267e2c7bc6d7
Rainer Burgstaller <rburgst@gmail.com>
2020-12-04 08:37:17 +0100

    add create product endpoint

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -4,8 +4,13 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import nc from 'next-connect'
 
 import connectDB from '../../../server/config/db'
+import {
+  isAdmin,
+  NextApiRequestWithUser,
+  protect,
+} from '../../../server/middlewares/authMiddleware'
 import { onError } from '../../../server/middlewares/index'
-import Product from '../../../server/models/productModel'
+import Product, { IProduct } from '../../../server/models/productModel'
 
 const getAllProducts = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   console.error('get products')
@@ -16,4 +21,17 @@ const getAllProducts = async (req: NextApiRequest, res: NextApiResponse): Promis
   res.json(products)
 }
 
-export default nc({ onError: onError }).get(getAllProducts)
+const createProduct = async (req: NextApiRequestWithUser, res: NextApiResponse): Promise<void> => {
+  console.error('create product')
+  await connectDB()
+
+  const newProduct = req.body as IProduct
+  newProduct.user = req.user
+
+  const order = new Product(newProduct)
+
+  const createdProduct = await order.save()
+  res.json(createdProduct)
+}
+
+export default nc({ onError: onError }).get(getAllProducts).post(protect, isAdmin, createProduct)
